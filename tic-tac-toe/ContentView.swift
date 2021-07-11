deab1170a7913f4c8dc4fc3312136c9101614916
Ya-Chieh Lai <yachieh@gmail.com>
2021-07-10 21:40:48 -0700

    Initial Commit

---
+++ tic-tac-toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tic-tac-toe
+//
+//  Created by Sabrina Hsieh on 7/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Sabrina Hsieh on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
