5789321a351ce1138bc2739dc5b3679e9394dae9
Ya-Chieh Lai <yachieh@gmail.com>
2021-07-17 00:24:19 -0700

    Removed use of closures, GeometryReader, and LazyVGrid to make it easier to explain functionality

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -11,63 +11,41 @@ import SwiftUI
 
 
 struct ContentView: View {
-
-    let columns: [GridItem] = [GridItem(.flexible()),
-                               GridItem(.flexible()),
-                               GridItem(.flexible())]
-
+    
     // empty game board is 9 nils
+    // a nil means it is empty (as versus '0' or 'false' which are specifc values)
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
-//    @State private var isHumansTurn = true
+    
     @State private var isGameBoardDisabled = false
     @State private var alertItem: AlertItem?
     
     var body: some View {
-        GeometryReader { geometry in
+        VStack {
+            Spacer()
+            Text("Tic Tac Toe")
             VStack {
-                Spacer()
-                LazyVGrid(columns:columns, spacing: 5){
-                    ForEach(0..<9) {i in
-                        ZStack{
-                            Circle()
-                                .foregroundColor(.red).opacity(0.5)
-                                .frame(width: geometry.size.width/3 - 15,
-                                       height: geometry.size.width/3 - 15)
-                            Image(systemName: moves[i]?.indicator ?? "")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.white)
-                        }
-                        .onTapGesture {
-                            if isSquareOccupied(in: moves, forIndex: i) { return }
-                            moves[i] = Move(player: .human, boardIndex: i)
-                                    
-//                            moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
-//                            isHumansTurn.toggle()
-                            
-                            // check for win condition or draw
-                            if checkWinCondition(for: .human, in: moves) {
-                                alertItem = AlertContext.humanWin
-                                return
+                ForEach(0..<3) { column in
+                    HStack {
+                        Spacer()
+                        ForEach(0..<3) { row in
+                            let i = 3*column + row
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.blue).opacity(0.5)
+                                    // aspect ratio of 1.0 ensures each rectangle is square
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                Text(moves[i]?.indicator ?? " ")
+                                    // create giant font size and then let it scale itself down
+                                    .font(.system(size: 500))
+                                    .minimumScaleFactor(0.01)
                             }
-                            if checkForDraw(in: moves) {
-                                alertItem = AlertContext.draw
-                                return
-                            }
-
-                            // lock the board once human has moved (computer's turn)
-                            isGameBoardDisabled = true
-
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                let computerPosition = determineComputerMovePosition(in: moves)
-                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
-                                
-                                // unlock the board once the computer has moved (humans' turn)
-                                isGameBoardDisabled = false
+                            .onTapGesture {
+                                if isSquareOccupied(in: moves, forIndex: i) { return }
+                                moves[i] = Move(player: .human, boardIndex: i)
                                 
                                 // check for win condition or draw
-                                if checkWinCondition(for: .computer, in: moves) {
-                                    alertItem = AlertContext.computerWin
+                                if checkWinCondition(for: .human, in: moves) {
+                                    alertItem = AlertContext.humanWin
                                     return
                                 }
                                 if checkForDraw(in: moves) {
@@ -75,26 +53,51 @@ struct ContentView: View {
                                     return
                                 }
                                 
+                                // lock the board once human has moved (computer's turn)
+                                isGameBoardDisabled = true
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    let computerPosition = determineComputerMovePosition(in: moves)
+                                    moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                    
+                                    // unlock the board once the computer has moved (humans' turn)
+                                    isGameBoardDisabled = false
+                                    
+                                    // check for win condition or draw
+                                    if checkWinCondition(for: .computer, in: moves) {
+                                        alertItem = AlertContext.computerWin
+                                        return
+                                    }
+                                    if checkForDraw(in: moves) {
+                                        alertItem = AlertContext.draw
+                                        return
+                                    }
+                                    
+                                }
                             }
                         }
+                        Spacer()
                     }
                 }
-                Spacer()
             }
+            // aspect ratio of 1.0 enures our 3x3 is square
+            .aspectRatio(1.0, contentMode: .fit)
             .disabled(isGameBoardDisabled)
-            .padding()
             .alert(item: $alertItem, content: {alertItem in
                 Alert(title: alertItem.title,
                       message: alertItem.message,
                       dismissButton: .default(alertItem.buttonTitle, action: { resetGame() }))
             })
+            Spacer()
         }
+        .padding()
     }
-
+    
     func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
-        return moves.contains(where: { $0?.boardIndex == index})
+        if moves[index] != nil {return true}
+        return false
     }
-
+    
     // AI is just doing something random
     // BUT
     // A better AI Strategy:
@@ -113,19 +116,35 @@ struct ContentView: View {
     }
 
     func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
-        let winPatterns: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8],
-                                          [0,3,6], [1,4,7], [2,5,8],
-                                          [0,4,8], [2,4,6]]
+        let winPatterns: [[Int]] = [[0,1,2], [3,4,5], [6,7,8],
+                                    [0,3,6], [1,4,7], [2,5,8],
+                                    [0,4,8], [2,4,6]]
         
-        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
-        let playerPostions = Set(playerMoves.map { $0.boardIndex })
-        
-        for pattern in winPatterns where pattern.isSubset(of: playerPostions) { return true }
+        for pattern in winPatterns {
+            // for all winPatterns, check to see if player has chosen all 3 winning slots
+            if (moves[pattern[0]]?.player == player) &&
+                (moves[pattern[1]]?.player == player) &&
+                (moves[pattern[2]]?.player == player) {
+                return true
+            }
+        }
         return false
     }
-    
+ 
     func checkForDraw(in moves: [Move?]) -> Bool {
-        return moves.compactMap { $0 }.count == 9
+        // draw if all possible moves have been taken
+        if (moves[0] != nil) &&
+            (moves[1] != nil) &&
+            (moves[2] != nil) &&
+            (moves[3] != nil) &&
+            (moves[4] != nil) &&
+            (moves[5] != nil) &&
+            (moves[6] != nil) &&
+            (moves[7] != nil) &&
+            (moves[8] != nil) {
+            return true
+        }
+        return false
     }
     
     func resetGame() {
@@ -142,7 +161,7 @@ struct Move {
     let boardIndex: Int
     
     var indicator: String {
-        return player == .human ? "xmark" : "circle"
+        return player == .human ? "X" : "O"
     }
 }
 
