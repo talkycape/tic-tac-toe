64e9f6dcc34db81a06cfa352ff9778315c307cb5
Y. Lai <yachieh@gmail.com>
2022-12-26 00:54:31 -0800

    Add drawn GUI elements - grid, Xs, and Os.

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -6,6 +6,8 @@
 //  With thanks to Sean Allen for his youtube example
 //  https://www.youtube.com/watch?v=MCLiPW2ns2w
 //
+//  Incorporating UI elements from the SwiftUI Cookbook by Scalzo and Nzokwe
+//
 
 import SwiftUI
 
@@ -22,6 +24,49 @@ struct Move {
     }
 }
 
+struct Nought: View {
+    var body: some View {
+        Circle()
+            .stroke(.red, lineWidth: 10)
+    }
+}
+
+struct CrossShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path() { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
+struct Cross: View {
+    var body: some View {
+        CrossShape()
+            .stroke(.green, style:  StrokeStyle(lineWidth: 10,
+                                                lineCap: .round,
+                                                lineJoin: .round))
+    }
+}
+
+struct GridShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path() { path in
+            path.move(to: CGPoint(x: rect.width/3, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.width/3, y: rect.maxY))
+            path.move(to: CGPoint(x: 2*rect.width/3, y: rect.minY))
+            path.addLine(to: CGPoint(x: 2*rect.width/3, y: rect.maxY))
+            path.move(to: CGPoint(x: rect.minX, y: rect.height/3))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.height/3))
+            path.move(to: CGPoint(x: rect.minX, y: 2*rect.height/3))
+            path.addLine(to: CGPoint(x: rect.maxX, y: 2*rect.height/3))
+
+        }
+    }
+}
+
 struct ContentView: View {
 
     // empty game board is 9 nils: nil means it is empty (as versus '0' or 'false' which are specifc values)
@@ -34,35 +79,42 @@ struct ContentView: View {
             Spacer()
             Text("Tic Tac Toe")
                 .font(.title)
-            VStack {
-                ForEach(0..<3) { column in
-                    HStack {
-                        Spacer()
-                        ForEach(0..<3) { row in
-                            let i = 3*column + row
-                            ZStack{
-                                Rectangle()
-                                    .foregroundColor(.blue).opacity(0.5)
-                                Text(moves[i]?.indicator ?? " ")
-                                    // create giant font size and then let it scale itself down
-                                    .font(.system(size: 500))
-                                    .minimumScaleFactor(0.01)
-                            }
-                            // aspect ratio of 1.0 ensures each rectangle is square
-                            .aspectRatio(1.0, contentMode: .fit)
-                            .onTapGesture {
-                                moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
-                                isHumansTurn.toggle()
+            Spacer()
+            ZStack {
+                GridShape()
+                    .stroke(.indigo, lineWidth: 15)
+                VStack {
+                    ForEach(0..<3) { column in
+                        HStack {
+                            ForEach(0..<3) { row in
+                                let i = 3*column + row
+                                ZStack {
+                                    Rectangle()
+                                        .fill(.clear)
+                                        .contentShape(Rectangle()) // allows the clear area to be tap-able
+                                    if (moves[i]?.indicator == "X") {
+                                        Cross()
+                                    }
+                                    else if (moves[i]?.indicator == "O") {
+                                        Nought()
+                                    }
+                                    else {
+                                        Color.clear
+                                    }
+                                }
+                                .padding(20)
+                                .onTapGesture {
+                                    moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
+                                    isHumansTurn.toggle()
+                                }
                             }
                         }
-                        Spacer()
                     }
                 }
             }
-            .aspectRatio(1.0, contentMode: .fit)
-            Spacer()
+            .padding(20)
         }
-        .padding()
+        .aspectRatio(1.0, contentMode: .fit)
     }
 }
  
